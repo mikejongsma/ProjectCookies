3114678cff19fdff60ece636dd0b6648a3c998fb
Mike Jongsma <mikejongsma@live.nl>
2019-05-16 16:04:22 +0200

    added logic with if else

---
+++ logic.js
@@ -0,0 +1,13 @@
+// Create the logic
+function agree(id){
+    let x = "agree";
+    let y = "disagree";
+    console.log(id);
+
+    if(id == "a"){
+        console.log(x);
+    }
+    if(id == "b"){
+        console.log(y);
+    }
+}
